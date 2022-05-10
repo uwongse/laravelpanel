d6b573e87e1651b727491c3947e40cac779f41a7
uwongse <trabajocmcmola@gmail.com>
2022-05-10 09:05:49 +0200

    Added REST Api final 14

--- database/migrations/2022_04_21_111420_create_permission_tables.php
+++ database/migrations/2022_04_21_111420_create_permission_tables.php
@@ -52,7 +52,7 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
             $table->unsignedBigInteger(PermissionRegistrar::$pivotPermission);
-
+            $table->bigIncrements('id');
             $table->string('model_type');
             $table->unsignedBigInteger($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
@@ -76,6 +76,7 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
             $table->unsignedBigInteger(PermissionRegistrar::$pivotRole);
+            $table->bigIncrements('id');
             $table->string('model_type');
             $table->unsignedBigInteger($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
