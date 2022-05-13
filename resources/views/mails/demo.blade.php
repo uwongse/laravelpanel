c5c33ba23b79cc3b0b2d47b56fb6d8fe0a4aa105
uwongse <trabajocmcmola@gmail.com>
2022-05-13 09:08:08 +0200

    Added REST Api final 73

--- resources/views/mails/demo.blade.php
+++ resources/views/mails/demo.blade.php
@@ -1,9 +1,8 @@
 
-<p>This is a demo email for testing purposes! Also, it's the HTML version.</p>
+<p>Has recibido un mensaje en el correo:</p>
  
-<p><u>Demo object values:</u></p>
-Thank You,
 <br/>
+<p>El nombre:</p>
 <i>{{ $demo['name'] }}</i>
 <div>
 <p><b>email:</b>&nbsp;{{ $demo['email'] }}</p>
