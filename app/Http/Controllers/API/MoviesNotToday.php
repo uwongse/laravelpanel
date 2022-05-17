a26d202b63c2cdb8dc3f0eff86428d4bb8cbc06b
uwongse <84908674+uwongse@users.noreply.github.com>
2022-05-17 17:57:33 +0200

    release_date por date 9

--- app/Http/Controllers/API/MoviesNotToday.php
+++ app/Http/Controllers/API/MoviesNotToday.php
@@ -24,7 +24,7 @@ class MoviesNotToday extends Controller
 
         return ProjectionIDResource::collection( Movie::whereHas('projections', function (Builder $query ) use ($id ,$date)  {
             $query->where('projections.syncronitation_id', $id->id)->where('release_date','>', $date);
-        })->join('projections', 'projections.movie_id', '=', 'movies.id')->orderBy('projections.release_date', 'asc')->orderBy('active', 'desc')->orderBy('premiere', 'asc')->with('Qualification')->with('Actor')->with('Director')
+        })->sortBy('projections.release_date', 'asc')->orderBy('active', 'desc')->orderBy('premiere', 'asc')->with('Qualification')->with('Actor')->with('Director')
         ->get());
     }
 
