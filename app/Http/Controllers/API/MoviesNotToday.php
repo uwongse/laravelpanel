c59859df3bcc17c636a6390a873c15f5df80b9d7
uwongse <84908674+uwongse@users.noreply.github.com>
2022-05-18 12:48:55 +0200

    api not today 13

--- app/Http/Controllers/API/MoviesNotToday.php
+++ app/Http/Controllers/API/MoviesNotToday.php
@@ -24,7 +24,9 @@ class MoviesNotToday extends Controller
 
         return ProjectionIDResource::collection( Movie::whereHas('projections', function (Builder $query ) use ($date)  {
             $query->where('release_date','>', $date);
-        })->with('projections')->with('Qualification')->with('Actor')->with('Director')->get()->sortBy('projections.release_date',SORT_REGULAR,false));
+        })->with('projections')->with('Qualification')->with('Actor')->with('Director')->get()->sortBy(function($movie, $key) {
+            return $movie->projections()->first()->release_date;
+          }));
     }
 
     /**
