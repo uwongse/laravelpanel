ba0ed4d6c68e0fb310c116943f6bd92118d80e46
uwongse <trabajocmcmola@gmai.com>
2022-05-21 23:00:41 +0200

    api vuelta

--- app/Http/Controllers/API/MoviesControler.php
+++ app/Http/Controllers/API/MoviesControler.php
@@ -24,7 +24,7 @@ class MoviesControler extends Controller
 
         return MoviesAllResource::collection( Movie::whereHas('projections', function (Builder $query ) use ($date) {
             $query->where('release_date','=',$date)->where('cinema_id', 2);
-        })->with('Qualification')->with('Actor')->with('Director')->get()->sortBy(function($movie, $key) {
+        })->orderBy('active', 'desc')->with('Qualification')->with('Actor')->with('Director')->get()->sortBy(function($movie, $key) {
             return $movie->projections()->first()->release_date;
           }));
     }
@@ -39,7 +39,7 @@ class MoviesControler extends Controller
       
         return MoviesAllResource::collection( Movie::whereHas('projections', function (Builder $query ) use ($date)  {
             $query->where('release_date','=',$date)->where('cinema_id', 1);
-        })->with('Qualification')->with('Actor')->with('Director')->get()->sortBy(function($movie, $key) {
+        })->orderBy('active', 'desc')->with('Qualification')->with('Actor')->with('Director')->get()->sortBy(function($movie, $key) {
             return $movie->projections()->first()->release_date;
           }));
     }
@@ -55,7 +55,7 @@ class MoviesControler extends Controller
 
         return MoviesAllResource::collection( Movie::whereHas('projections', function (Builder $query ) use ($date)  {
             $query->where('release_date','>=',$date)->where('cinema_id', 3);
-        })->with('Qualification')->with('Actor')->with('Director')->get()->sortBy(function($movie, $key) {
+        })->orderBy('active', 'desc')->with('Qualification')->with('Actor')->with('Director')->get()->sortBy(function($movie, $key) {
             return $movie->projections()->first()->release_date;
           }));
     }
